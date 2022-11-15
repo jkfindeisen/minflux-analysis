0b3d1bea5619631455b9182b50a2a0efe01b67ce
manerotoni <mail@apoliti.de>
2022-11-15 14:20:21 +0100

    Processing template

--- python/mfx/mfx/utilities.py
+++ python/mfx/mfx/utilities.py
@@ -1,8 +1,9 @@
 import os
 import glob
 import warnings
-from . import mfxdata
-
+from .mfxdata import MfxData
+from .processlocalizations import ProcessLocalizations
+from . import mfxcolnames as col
 def gen_directories(main_dir, sub_dir, keys):
     """
     Combine different path to directories
@@ -20,6 +21,8 @@ def gen_directories(main_dir, sub_dir, keys):
 def gen_msrfiles(dir_dict):
     """
     Per each path/key value in dir_dict create list of msr files
+    :param dir_dict: A dictionary of paths
+    :return: npy_files: A dictionary of file arrays
     """
     msr_files = {}
     for key in dir_dict:
@@ -28,6 +31,56 @@ def gen_msrfiles(dir_dict):
             warnings.warn("No msr files found in directory %s" % dir_dict[key])
         msr_files[key] = files
     return msr_files
+
+
+def gen_npyfiles(dir_dict):
+    """
+    Per each path/key value in dir_dict create list of npy files
+    :param dir_dict: A dictionary of paths
+    :return: npy_files: A dictionary of file arrays
+    """
+    npy_files = {}
+    for key in dir_dict:
+        npy_files[key] = []
+
+    for key in dir_dict:
+        for (root, dirs, files) in os.walk(dir_dict[key]):
+            if len(dirs) > 0:
+                for adir in dirs:
+                    npyfile = os.path.join(root, adir, adir + '.npy')
+                    if os.path.exists(npyfile):
+                        npy_files[key].append(npyfile)
+    return npy_files
+
+
+def process_mfx_npyfile(file_name, eps_range):
+    """
+    Example of workflow to trim and cluster localizations
+    :param file_name:
+    :param eps_range:
+    """
+    pl = ProcessLocalizations(file_name)
+    pl.MIN_LOCALIZATIONS = 3    # 3 is the default value
+    # Remove all tracks with less than MIN_LOCALIZATIONS
+    pl.trim_min_localizations()
+    # Split tracks if needed, remove outliers in each track
+    pl.cluster_tid(method=pl.CLS_METHOD_BAYES_GMM)
+
+    # find objects that belong to the same cluster according to distance 30 nm
+    for eps in eps_range:
+        pl.DBCLUSTER_EPS_MEAS = eps
+        pl.DBCLUSTER_EPS_ALL = eps
+        pl.DBCLUSTER_EPS_MERGED_ALL = eps
+        pl.DBCLUSTER_EPS_MERGED_MEAS = eps
+        pl.log_parameters()
+        pl.cluster_meas(method=pl.CLS_METHOD_DBSCAN)
+        pl.cluster_all(method=pl.CLS_METHOD_DBSCAN)
+        pl.cluster_all_intersect(col.CLS_ALL)
+        summary_dict = pl.summary_per_tid2()
+        eps_txt = '%.0f' % (eps*1e9)
+        pl.export_csv(summary_dict, file_path=pl.file_path_no_ext() + '_eps' + eps_txt)
+        pl.export_vtu(in_dict=summary_dict, coord=col.LTR,
+                      file_path=pl.file_path_no_ext() + '_eps' + eps_txt)
 
 
 # Realign data funcion
@@ -40,7 +93,7 @@ def align_to_ref_beads(msr_files, out_dir, zarr_dir, invalid_msr, exclude_beads,
             file_name = os.path.basename(file_path)
             if file_name in invalid_msr:
                 continue
-            mfx = mfxdata.MfxData(file_path, outdir_main=out_dir[wash_key],
+            mfx = MfxData(file_path, outdir_main=out_dir[wash_key],
                                   zarr_dir_main=zarr_dir[wash_key])
             if file_name in time_diff_ref:
                 mfx.MAX_TDIFF_REF = time_diff_ref[file_name]
@@ -74,7 +127,7 @@ def preview_ref_beads(msr_files: dict, out_dir: dict, zarr_dir: dict, id_file: i
     :param exclude_beads: List of BEAD IDs, e.g. ['R10', 'R01'] to exclude
     :return:
     """
-    mfx = mfxdata.MfxData(msr_files[key][id_file], outdir_main=out_dir[key],
+    mfx = MfxData(msr_files[key][id_file], outdir_main=out_dir[key],
                           zarr_dir_main=zarr_dir[key])
     print(mfx.msrfile_path)
     mfx.MAX_TDIFF_REF = time_diff_ref
